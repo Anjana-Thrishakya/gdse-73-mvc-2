7c482a23a1ca45bcae2a8f6e1c1c0b622621b39f
Anjana Thrishakya <anjanathrishakya95@gmail.com>
2025-04-03 11:34:08 +0530

    ItemDto

--- src/edu/ijse/mvc/Main.java
+++ src/edu/ijse/mvc/Main.java
@@ -4,7 +4,7 @@
  */
 package edu.ijse.mvc;
 
-import edu.ijse.mvc.db.DBConnection;
+import edu.ijse.mvc.view.ItemView;
 
 /**
  *
@@ -16,7 +16,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception{
-        DBConnection.getInstance().getConnection();
+        new ItemView().setVisible(true);
     }
     
 }
