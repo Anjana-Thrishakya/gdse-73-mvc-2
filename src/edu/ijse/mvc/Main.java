59750defc3da6f5b368e627b4c9d7d9bb7b889d6
Anjana Thrishakya <anjanathrishakya95@gmail.com>
2025-04-10 10:19:57 +0530

    Order UI Initiate

--- src/edu/ijse/mvc/Main.java
+++ src/edu/ijse/mvc/Main.java
@@ -6,6 +6,7 @@ package edu.ijse.mvc;
 
 import edu.ijse.mvc.view.CustomerView;
 import edu.ijse.mvc.view.ItemView;
+import edu.ijse.mvc.view.OrderView;
 
 /**
  *
@@ -17,7 +18,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception{
-        new CustomerView().setVisible(true);
+        new OrderView().setVisible(true);
     }
     
 }
